740accd607d139a505414c69f73925c8c21b6024
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-02-03 21:45:08 +0530

    Fix License

--- bugyou_plugins/commands/cntrl.py
+++ bugyou_plugins/commands/cntrl.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2012 Red Hat, Inc.
+# Copyright (C) 2015 Red Hat, Inc.
 #
-# fedmsg is free software; you can redistribute it and/or
+# bugyou_plugins is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
