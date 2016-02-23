bb3555f41448e6f66bc695a4f779b39b270aa84d
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-02-23 17:51:00 +0530

    Fix the LICENSE in the files

--- bugyou_plugins/utility.py
+++ bugyou_plugins/utility.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Red Hat, Inc.
+#
+# bugyou_plugins is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# bugyou_plugins is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# bugyou_plugins.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Sayan Chowdhury <sayanchowdhury@fedoraproject.org>
+#
+
 import os
 import ConfigParser
 import pkg_resources
-
 
 def load_config(filepath):
     """ Load the configuration file """
